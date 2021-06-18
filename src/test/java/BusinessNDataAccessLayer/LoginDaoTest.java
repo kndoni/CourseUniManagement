8390082ae7fcbc530d9b29ca1813808cd682744e
Kristi Ndoni <kristindoni1@gmail.com>
2021-06-18 12:27:46 +0200

    Modified the tests for LoginDao

--- src/test/java/BusinessNDataAccessLayer/LoginDaoTest.java
+++ src/test/java/BusinessNDataAccessLayer/LoginDaoTest.java
@@ -12,26 +12,28 @@ import org.junit.Test;
 
 /**
  *
- * @author user
+ * @author kristi , andrea, islam
  */
 public class LoginDaoTest {
     
     public static LoginDao loginDao = new LoginDao();
    
     /**
-     * Test of validate method, of class LoginDao.
+     * Test of validate method, of class LoginDao. Execution of two cases false and true.
      */
     @Test
     public void testValidateLoginFalse() {
-        LoginForm loginForm = new LoginForm();
-        boolean result = false;
-        Assert.assertEquals(result,loginForm.LoginUser("kristi", "kristi1233"));
+        LoginForm loginForm1 = new LoginForm();
+        boolean status1 = loginForm1.LoginUser("kristi", "kristi1233");
+        boolean result1 = false;
+        Assert.assertEquals(result1,status1);
     }
     
     @Test
     public void testValidateLoginTrue(){
-        LoginForm loginForm = new LoginForm();
-        boolean result = true;
-        Assert.assertEquals(result,loginForm.LoginUser("kristi", "kristi123"));
+        LoginForm loginForm2 = new LoginForm();
+        boolean status2 = loginForm2.LoginUser("kristi", "kristi123");
+        boolean result2 = true;
+        Assert.assertEquals(result2,status2);
     }
 }
