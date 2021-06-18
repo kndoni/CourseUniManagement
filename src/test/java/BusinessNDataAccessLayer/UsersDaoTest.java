878d592d2327321a4b963da9d9f49e8f7086426b
Kristi Ndoni <kristindoni1@gmail.com>
2021-06-18 12:28:56 +0200

    Created integration test for sign up users functionality

---
+++ src/test/java/BusinessNDataAccessLayer/UsersDaoTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessNDataAccessLayer;
+
+import PresentationLayer.SignUpFrame;
+import com.mysql.jdbc.PreparedStatement;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author user
+ */
+public class UsersDaoTest {
+    
+    public static UsersDao usersDao = new UsersDao();
+
+    /**
+     * Test of saveUser method, of class UsersDao.
+     */
+    @Test
+    public void testSaveUser() {
+        UsersDao usersDao = new UsersDao();
+        SignUpFrame signUp1 = new SignUpFrame();
+        boolean value1 = signUp1.signUpUser("user1", "user1", "user1 user1", "user1@email.com", 21, "major1");
+        
+        SignUpFrame signUp2 = new SignUpFrame();
+        boolean value2 = signUp2.signUpUser("user2", "user2", "user2 user2", "user2@email.com", 21, "major2");
+        
+        SignUpFrame signUp3 = new SignUpFrame();
+        boolean value3 = signUp3.signUpUser("user2", "user2", "user2 user2", "user2@email.com", 21, "major2");
+        
+        boolean result1 = UsersDao.CheckIfAlready("user1");
+        boolean result2 = UsersDao.CheckIfAlready("user2");
+        boolean result3 = UsersDao.CheckIfAlready("user3");
+        
+        if(result1 == false)
+            result1 =true;
+        if(result2 == false);
+            result2=true;
+        if(result3 == false)
+            result3=true;
+        
+         Assert.assertEquals(result3, value3);
+         Assert.assertEquals(result1, value1);
+         Assert.assertEquals(result2, value2);
+    }
+    
+    @After
+    public void cleanDatabase() {
+	try {
+	    PreparedStatement ps = (PreparedStatement) UsersDao.getC().prepareStatement("delete from login");
+	    ps.execute();
+	} catch (SQLException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+		}
+    }
+
+    @After
+    public void closeConnection() {
+	usersDao.close();
+    }
+    
+}
